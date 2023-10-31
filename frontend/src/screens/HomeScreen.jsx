f2e24d43f5e796d29676f6fcb86633821ad9e721
Andriy Polukhin <andriy.polukhin@gmail.com>
2023-10-31 16:35:29 +0200

    created HomeScreen, created Product component, added product to the HomeScreen

---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,17 @@
+import { Row, Col } from 'react-bootstrap'
+import products from '../products'
+import Product from '../components/Product'
+const HomeScreen = () => (
+	<>
+		<h1>Latest Products</h1>
+		<Row>
+			{products.map(product => (
+				<Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+					<Product product={product} />
+				</Col>
+			))}
+		</Row>
+	</>
+)
+
+export default HomeScreen
