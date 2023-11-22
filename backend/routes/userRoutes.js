8eaa498012b7142065a12237faa8b54971ad70bd
Andriy Polukhin <andriy.polukhin@gmail.com>
2023-11-22 22:43:23 +0200

    added order routes and order controller

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,5 @@
 import express from 'express'
 const router = express.Router()
-import asyncHandler from '../middleware/asyncHandler.js'
-import User from '../models/userModel.js'
 import {
 	authUser,
 	registerUser,
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ dotenv.config()
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 const port = process.env.PORT || 5000
@@ -19,12 +20,13 @@ app.use(express.urlencoded({ extended: true })) // * encoded json
 // * Cookie parser middleware
 app.use(cookieParser())
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
 	res.send('API is running')
 })
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
