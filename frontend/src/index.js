428017ba51a01657040d6cce4143147ffdb8c97b
Andriy Polukhin <andriy.polukhin@gmail.com>
2023-11-26 22:20:00 +0200

    feat(screen): user list screen added, user can be deleted

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -29,6 +29,7 @@ import ProfileScreen from './screens/ProfileScreen'
 import OrderListScreen from './screens/admin/OrderListScreen'
 import ProductListScreen from './screens/admin/ProductListScreen'
 import ProdcutEditScreen from './screens/admin/ProdcutEditScreen'
+import UserListScreen from './screens/admin/UserListScreen'
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
@@ -49,6 +50,7 @@ const router = createBrowserRouter(
 				<Route path='/admin/orderlist' element={<OrderListScreen />} />
 				<Route path='/admin/productlist' element={<ProductListScreen />} />
 				<Route path='/admin/product/:id/edit' element={<ProdcutEditScreen />} />
+				<Route path='/admin/userlist' element={<UserListScreen />} />
 			</Route>
 		</Route>
 	)
